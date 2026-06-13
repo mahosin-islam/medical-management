b81c1e2b5bb8391bb755f7166e98a988d13da2c5
mahsoin <mahosin.dev@gmail.com>
2026-06-13 19:28:23 +0600

    add new futur doctor pae

--- src/app/(marketing)/doctors/page.tsx
+++ src/app/(marketing)/doctors/page.tsx
@@ -2,8 +2,11 @@
 function Doctor() {
   return (
     <div className="h-screen">
-  <h2>helle Docrot jara</h2>
-
+          <div className="py-10 pl-10">
+            <h2>Docto naem Mahosin</h2>
+         <h3>Desicnation: 4040</h3>
+        <h4>MMBS CSP Dhaka</h4>
+          </div>
     </div>
   )
 }

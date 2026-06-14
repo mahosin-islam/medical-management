1474ffc31176fdfe8263a434defd92636f2a66b3
mahsoin <mahosin.dev@gmail.com>
2026-06-14 06:35:03 +0600

    doctor erro fixed

--- src/app/(marketing)/doctors/page.tsx
+++ src/app/(marketing)/doctors/page.tsx
@@ -3,8 +3,8 @@ function Doctor() {
   return (
     <div className="h-screen">
           <div className="py-10 pl-10">
-            <h2>Doctor  naem Mahosin</h2>
-            <h2>{"Mahosin's Project"} Madical</h2>
+            <h2>Doctor  naem's Mahosin</h2>
+           
           </div>
     </div>
   )

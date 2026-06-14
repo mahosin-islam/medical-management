07bc6bbfb0daea2305f5435bf073883a0be15956
mahsoin <mahosin.dev@gmail.com>
2026-06-14 06:27:26 +0600

    doctor page modify

--- src/app/(marketing)/doctors/page.tsx
+++ src/app/(marketing)/doctors/page.tsx
@@ -4,8 +4,7 @@ function Doctor() {
     <div className="h-screen">
           <div className="py-10 pl-10">
             <h2>Doctor  naem Mahosin</h2>
-         <h3>Desicnation: 4040</h3>
-        <h4>MMB&s alndo usa</h4>
+            <h2>{"Mahosin's Project"} Madical</h2>
           </div>
     </div>
   )

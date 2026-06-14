817de9401bf2ab0386e4b742a1b7ef3da5fa9333
mahsoin <mahosin.dev@gmail.com>
2026-06-14 06:11:13 +0600

    change envronment variable

--- src/app/(marketing)/doctors/page.tsx
+++ src/app/(marketing)/doctors/page.tsx
@@ -5,7 +5,7 @@ function Doctor() {
           <div className="py-10 pl-10">
             <h2>Doctor  naem Mahosin</h2>
          <h3>Desicnation: 4040</h3>
-        <h4>MMB;S CSP Dhaka</h4>
+        <h4>MMB&s alndo usa</h4>
           </div>
     </div>
   )
